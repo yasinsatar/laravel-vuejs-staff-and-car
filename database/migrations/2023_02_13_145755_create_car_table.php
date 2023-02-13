58aa1f970e978f2c2f13dfe8fcc49ece09151d87
Yasin Satar <yasinsatar@hotmail.com>
2023-02-13 18:07:54 +0300

    Migration oluşturulması

---
+++ database/migrations/2023_02_13_145755_create_car_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car', function (Blueprint $table) {
+            $table->id();
+            $table->string('car_name');
+            $table->string('car_plate')->unique();
+            $table->string('car_model');
+            $table->string('car_info');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car');
+    }
+};
